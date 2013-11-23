35c49885d86cb3d5ee74b20e81609d26a6a5ca28
Pavan Sudheendra <pavan0591@gmail.com>
2013-11-23 23:32:08 +0530

    Got all the saved links from 91_pavan. Made changes

--- redditsaver.py
+++ redditsaver.py
@@ -1,11 +1,12 @@
 __author__ = 'pavan'
 import praw
+from pprint import pprint
 
 user_agent = ('exporting saved links from one username to another username v1.0 by /u/91_pavan')
 r = praw.Reddit(user_agent=user_agent)
-r.login('91_pavan', 'xxxxxxxxx')
-saved_links = r.user.get_saved(limit=None)
+username = '91_pavan'
+password = ''
+r.login(username, password)
+saved_links = r.user.get_saved(limit=50)
 
 
-for x in saved_links:
-    print x
