ad21a25fae925206ddb1a06631b785ad0662455b
Pavan Sudheendra <pavan0591@gmail.com>
2013-12-18 01:00:31 +0530

    Counts number of saved links

--- reddit_saved_count.py
+++ reddit_saved_count.py
@@ -5,7 +5,7 @@ import traceback
 user_agent = ('exporting saved links from one username to another username v1.0 by /u/91_pavan')
 r = praw.Reddit(user_agent=user_agent)
 username = '91_pavan'
-password = 'pavansameera'
+password = ''
 r.login(username, password)
 saved_links = r.user.get_saved(limit=None)
 count = 0
