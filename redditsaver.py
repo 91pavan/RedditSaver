72844ccd0a6a1dbe8030a5b70dff2436cb2be8c0
Pavan Sudheendra <pavan0591@gmail.com>
2013-12-18 00:55:47 +0530

    Succeffully transferred saved links from one user to another

--- redditsaver.py
+++ redditsaver.py
@@ -1,13 +1,38 @@
 __author__ = 'pavan'
 import praw
 from pprint import pprint
-
-user_agent = ('exporting saved links from one username to another username v1.0 by /u/91_pavan')
+import traceback
+user_agent = ('exporting saved links from one username to another username v1.1 by /u/91_pavan')
 r = praw.Reddit(user_agent=user_agent)
 username = '91_pavan'
 password = ''
 r.login(username, password)
-saved_links = r.user.get_saved(limit=50)
-for x in saved_links:
-    print str(x.url)
+saved_links = r.user.get_saved(limit=None)
+post = iter(saved_links)
+list = []
+try:
+    for i in post:
+        list.append(i.id)
+except Exception, e:
+    traceback.print_exc()
+    #list.append(i.id)
 
+#print list
+
+
+#print list
+user_agent1 = ('exporting saved links from one username to another username v1.1 by /u/icutyouwithmmyknife')
+r1 = praw.Reddit(user_agent = user_agent1)
+username1 = 'icutyouwithmyknife'
+password1 = ''
+r1.login(username1, password1)
+
+try:
+    for entry in list:
+        submission = r1.get_submission(url=None, submission_id=entry, comment_limit=0, comment_sort=None)
+        submission.unsave()
+except:
+    pass
+
+
+print "All Done!"
